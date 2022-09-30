68b8140efdef7bab0c235ee6464c6205cb80ac1a
“kashsan-jp” <kashsan_jp@yahoo.co.jp>
2022-09-30 16:05:48 +0900

    edit8

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", e => {
-    //console.log("Install!");    
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", "./css/style.css", "./img/icon-192x192.png"]);
-        })
-    );
-}); 
